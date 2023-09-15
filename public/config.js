cf97be74d294471a9989679203be99c4c1b38ad7
sooyaa2000 <130429781+sooyaa2000@users.noreply.github.com>
2023-09-15 22:29:37 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,5 +2,5 @@ window.__PRE_CONFIG__ = {
   header: 'SdSjm-Server',
   subHeader: '探针服务器/ServerStatus',
   interval: 1.5,
-  footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
+  footer: '<p> by <a href="https://server.sdsjm.top/">SdSjm</a></p>'
 };
