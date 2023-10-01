786035adac571b57a56de7492b7b39e71661e2eb
sooyaa2000 <130429781+sooyaa2000@users.noreply.github.com>
2023-10-01 23:11:24 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__PRE_CONFIG__ = {
-  header: 'SdSjm-Server',
-  subHeader: '探针服务器/ServerStatus',
+  header: 'ServerStatus-SdSjm',
+  subHeader: '探针服务/自己的小鸡',
   interval: 1.5,
   footer: '<p> by <a href="https://server.sdsjm.top/">SdSjm</a></p>'
 };
